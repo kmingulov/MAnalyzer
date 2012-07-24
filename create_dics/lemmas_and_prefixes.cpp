3f52d0d18550ec2bba8dec298a65fb776a080c77
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2012-07-24 12:39:44 +0700

    Add dics creation sources (lemmas and prefixes).

---
+++ create_dics/lemmas_and_prefixes.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <dawgdic/dawg-builder.h>
+#include <dawgdic/dictionary-builder.h>
+using namespace std;
+using namespace dawgdic;
+
+// Creates a dictionary from lexicon file.
+void create_dic(const char * input_file, const char * output_file)
+{
+    ifstream input(input_file);
+    DawgBuilder dawg_builder;
+
+    // Reads words from file stream and adds them to the DAWG.
+    string temp;
+    int counter = 0;
+    while(input >> temp)
+        dawg_builder.Insert(temp.c_str(), counter++);
+
+    // Finishes building a dawg.
+    Dawg dawg;
+    dawg_builder.Finish(&dawg);
+
+    // Builds a dictionary a dawg.
+    Dictionary dic;
+    DictionaryBuilder :: Build(dawg, &dic);
+
+    // Writes a dictionary into a file.
+    ofstream output(output_file, ios::binary);
+    dic.Write(&output);
+}
+
+int main()
+{
+    create_dic("src/predict_prefixes", "../dics/predict_prefixes.dic");
+    create_dic("src/lemmas", "../dics/lemmas.dic");
+
+    return 0;
+}
