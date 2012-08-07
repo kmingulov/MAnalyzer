ab2c0ea1a09dc5973d7af4a6079f22638e6a830f
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2012-08-07 19:06:08 +0700

    Final fix for endings_rules and endings lexicon creation.

--- create_dics/make_dawgdics.cpp
+++ create_dics/make_dawgdics.cpp
@@ -2,25 +2,11 @@
 #include <algorithm>
 #include <dawgdic/dawg-builder.h>
 #include <dawgdic/dictionary-builder.h>
+
+#include <cstdlib>
+
 using namespace std;
 using namespace dawgdic;
-
-// Reverse each word in file.
-void reverse_file(const char * input_file, const char * output_file)
-{
-    ifstream input(input_file);
-    ofstream output(output_file);
-
-    string temp;
-    while(input >> temp)
-    {
-        reverse(temp.begin(), temp.end());
-        output << temp << endl;
-    }
-
-    input.close();
-    output.close();
-}
 
 // Creates a dictionary from lexicon file.
 void create_dic(const char * input_file, const char * output_file)
@@ -49,19 +35,9 @@ void create_dic(const char * input_file, const char * output_file)
 
 int main()
 {
-    // Prepare endings lexicon.
-    // Reverse file (in cp-1251 encoding or another 1-byte encoding).
-    reverse_file("temp/endings", "temp/endings_reversed");
-    // Recode to UTF8 (for sort).
-    system("recode -f cp1251..utf8 temp/endings_reversed");
-    // Sort.
-    system("sort -o temp/endings_sorted temp/endings_reversed");
-    // Recode to cp-1251.
-    system("recode -f utf8..cp1251 temp/endings_sorted");
-
     create_dic("temp/predict_prefixes_sorted", "../dics/predict_prefixes.dawgdic");
     create_dic("temp/lemmas", "../dics/lemmas.dawgdic");
-    create_dic("temp/endings_sorted", "../dics/endings.dawgdic");
+    create_dic("temp/ends", "../dics/endings.dawgdic");
 
     return 0;
 }
